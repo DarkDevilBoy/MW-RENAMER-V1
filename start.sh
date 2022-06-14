6467c1d9f11b3d09f5c42ff2a85e093198a3b2a9
DarkDevilBoy <98202785+DarkDevilBoy@users.noreply.github.com>
2022-06-14 19:17:19 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/DarkDevilBoy/MW-RENAMER-V1.git /MW-RENAMER-V1
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /MW-RENAMER-V1
 fi
-cd /EvaMaria
+cd /MW-RENAMER-V1
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
